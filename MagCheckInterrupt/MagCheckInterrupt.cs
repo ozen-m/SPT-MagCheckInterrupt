8ec7d27b260415a94d4f354d656ccdf28253c11e
ozen-m <74519617+ozen-m@users.noreply.github.com>
2026-09-10 15:07:35 +0800

    LICENSE

--- MagCheckInterrupt/MagCheckInterrupt.cs
+++ MagCheckInterrupt/MagCheckInterrupt.cs
@@ -1,4 +1,22 @@
-﻿using BepInEx;
+﻿/*
+    Magazine Check Interrupt - Seamlessly transition from a magazine check to a reload!
+    Copyright (C) 2026  ozen-m
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using BepInEx;
 using BepInEx.Bootstrap;
 using EFT.InputSystem;
 using HarmonyLib;
